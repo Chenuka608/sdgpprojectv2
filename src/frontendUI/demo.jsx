a09c17f2bfbf5187601105c11783594e69c8c391
Chenuka <136786624+Chenuka608@users.noreply.github.com>
2024-03-25 07:18:50 +0530

    working tree fixed

--- src/frontendUI/demo.jsx
+++ src/frontendUI/demo.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import backgroundImage from '../images/mic1.jpg';
-
-const InterviewDemo = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top when the component mounts
-  }, []);
-
-  return (
-    <div className="container mx-auto py-80 relative bg-contain" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <h1 className="text-5xl font-bold mb-4 text-[#0C024B]"> Interview Demo</h1>
-      <div className="grid grid-cols-2 gap-8">
-        {/* Audio Input Section */}
-        
-        {/* Feedback Section */}
-        <div style={{ marginLeft: 'auto' }}> {/* Adjust marginRight as needed */}
-          <h2 className="text-xl font-bold mb-2">Feedback</h2>
-          {/* Placeholder for feedback message */}
-          <p className="text-gray-600">Awaiting input....</p>
-=======
 import React, { useEffect, useRef } from 'react';
 import backgroundImage from '../images/mic1.jpg';
 
@@ -48,7 +27,6 @@ const InterviewDemo = () => {
             <button type="submit">Predict</button>
           </form>
           <div id="predictionResult"></div>
->>>>>>> 0b4aca2232524c2a204d04e3910b5e664e9b17af
         </div>
       </div>
     </div>
