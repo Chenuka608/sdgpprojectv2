2f675803dd83e826b0a645cc04ebd20056044d45
Chenuka <136786624+Chenuka608@users.noreply.github.com>
2024-03-25 06:58:39 +0530

    Update demo.jsx
    fixed error demo

--- src/frontendUI/demo.jsx
+++ src/frontendUI/demo.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from 'react';
+<<<<<<< HEAD
+import React, { useEffect } from 'react';
 import backgroundImage from '../images/mic1.jpg';
 
 const InterviewDemo = () => {
@@ -6,6 +7,22 @@ const InterviewDemo = () => {
     window.scrollTo(0, 0); // Scroll to the top when the component mounts
   }, []);
 
+  return (
+    <div className="container mx-auto py-80 relative bg-contain" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <h1 className="text-5xl font-bold mb-4 text-[#0C024B]"> Interview Demo</h1>
+      <div className="grid grid-cols-2 gap-8">
+        {/* Audio Input Section */}
+        
+        {/* Feedback Section */}
+        <div style={{ marginLeft: 'auto' }}> {/* Adjust marginRight as needed */}
+          <h2 className="text-xl font-bold mb-2">Feedback</h2>
+          {/* Placeholder for feedback message */}
+          <p className="text-gray-600">Awaiting input....</p>
+=======
+import React, { useEffect, useRef } from 'react';
+import backgroundImage from '../images/mic1.jpg';
+
+const InterviewDemo = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -22,7 +39,7 @@ const InterviewDemo = () => {
 
   return (
     <div className="container mx-auto py-80 relative bg-contain" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <h1 className="text-5xl font-bold mb-4 text-[#0C024B]"> Interview Demo</h1>
+      <h1 className="text-5xl font-bold mb-4 text-[#0C024B]"> Emotion Prediction</h1>
       <div className="grid grid-cols-2 gap-8">
         {/* Audio Input Section */}
         <div>
@@ -31,12 +48,7 @@ const InterviewDemo = () => {
             <button type="submit">Predict</button>
           </form>
           <div id="predictionResult"></div>
-        </div>
-        {/* Feedback Section */}
-        <div style={{ marginLeft: 'auto' }}> {/* Adjust marginRight as needed */}
-          <h2 className="text-xl font-bold mb-2">Feedback</h2>
-          {/* Placeholder for feedback message */}
-          <p className="text-gray-600">Awaiting input....</p>
+>>>>>>> 0b4aca2232524c2a204d04e3910b5e664e9b17af
         </div>
       </div>
     </div>
